2dffb6a68f37d6c63160c733c278c48a4aa58e2c
Chimba <chimbeevaa@gmail.com>
2023-01-07 19:19:25 +0800

    070122

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-"use strict";
 /*скрипт должен выполнять следующее
 При начале ввода в поле ввода становиться активной кнопка "Отправить"
 При начале ввода отображается счетчик введенных символов
@@ -7,9 +6,6 @@
 и кнопка "Отправить" не активна)
 При нажатии кнопки "Отправить" комментарий перемещается в блок divCommentOutput
  */
-exports.__esModule = true;
-//import {CommentObj} from "./CommentObj.js";
-var CommentObj_1 = require("./CommentObj");
 var container = document.querySelector(".formInput");
 var area = document.getElementById('comment');
 if (area != null) {
@@ -35,8 +31,76 @@ if (area != null) {
 ;
 var areaButton = document.getElementById('divButton'); // Обработка нажатия кнопки
 areaButton.addEventListener('click', function (event) {
-    var areaList = document.getElementById('divCommentOutputWrapper');
-    var comObj = new CommentObj_1["default"]();
+    //  const areaList = document.getElementById('divCommentOutputWrapper') as HTMLElement;
+    var comObj = new CommentObj();
     comObj.render();
-    //areaList.appendChild("comObj.")
+    //areaList.appendChild(divCommentInput);
 });
+var CommentObj = /** @class */ (function () {
+    function CommentObj() {
+        // Создаем объект с параметрами комментария.
+        this.timeOfComment = new Date(); // сохраняем время ввода комментария.
+        var elementAvatar = document.querySelector(".divAvatar");
+        this.avatar = elementAvatar.getAttribute("background-image");
+        this.nickName = document.getElementById("divNickName").textContent;
+        this.textOfComment = document.getElementById("comment").value;
+    }
+    CommentObj.prototype.render = function () {
+        var divCommentInput = document.createElement("divCommentInput");
+        divCommentInput.className = "divCommentInput";
+        var divAvatar = document.createElement("divAvatar");
+        //divAvatar.style.backgroundImage = "url('https://picsum.photos/85/128')";
+        divAvatar.className = "divAvatar";
+        divCommentInput.appendChild(divAvatar);
+        var divCommentInputCenter = document.createElement("divCommentInputCenter");
+        divCommentInputCenter.className = "divCommentInputCenter";
+        divCommentInput.appendChild(divCommentInputCenter);
+        var divCommentInputTop = document.createElement("divCommentInputTop");
+        divCommentInputTop.className = "divCommentInputTop";
+        divCommentInputCenter.appendChild(divCommentInputTop);
+        var divNickName = document.createElement("divNickName");
+        divNickName.innerHTML = this.nickName;
+        var divCounter = document.createElement("divCounter");
+        var divWarning = document.createElement("divWarning");
+        divCommentInputTop.appendChild(divNickName);
+        divCommentInputTop.appendChild(divCounter);
+        divCommentInputTop.appendChild(divWarning);
+        var divCommentInputBottom = document.createElement("divCommentInputBottom");
+        divCommentInputBottom.className = "divCommentInputBottom";
+        divCommentInputCenter.appendChild(divCommentInputBottom);
+        var textComment = document.createElement("textComment");
+        textComment.innerHTML = this.textOfComment;
+        divCommentInputBottom.appendChild(textComment);
+        var divOutputWrapper = document.querySelector(".divCommentOutputWrapper");
+        divOutputWrapper.appendChild(divCommentInput);
+        return;
+        /*
+            <div class="divCommentInput">
+                <div class="divAvatar"></div>
+                <div class="divCommentInputCenter">
+                    <div class="divCommentInputTop">
+                        <div class="divNickName" id="divNickName">
+                            <p style="display: inline">Максим Авдеенко</p>
+                        </div>
+                        <div class="divCounter">
+                            Макс. 1000 символов
+                        </div>
+                        <div class="divWarning"></div>
+                    </div>
+                    <div class="divCommentInputBottom">
+                        <form action="main.js" class="formInput" id="commentForm">
+                            <textarea datatype="text" class="textComment" placeholder="Введите текст сообщения"
+                            name="comment" id="comment" cols="40" rows="3" required form="commentForm"></textarea>
+                            <input class="divButton" id="divButton" type="submit" value="Отправить" disabled>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        */
+    };
+    return CommentObj;
+}());
+//export class ListOfComments extends CommentObj{ // Сущность, которая хранит список комментариев.
+//  ListOfComments: { id: number, avatar: string, nickName: string, textOfComment: string, timeOfComment: Date }[]
+//}
+//export default CommentObj;
--- main.ts
+++ main.ts
@@ -7,13 +7,7 @@
 При нажатии кнопки "Отправить" комментарий перемещается в блок divCommentOutput
  */
 
-//import {CommentObj} from "./CommentObj.js";
-
-
-import CommentObj from "./CommentObj";
-
 const container: HTMLElement = document.querySelector(".formInput");
-
 const area = document.getElementById('comment') as HTMLInputElement | null;
     if (area != null) {
 
@@ -37,8 +31,87 @@ const area = document.getElementById('comment') as HTMLInputElement | null;
 
 const areaButton = document.getElementById('divButton') as HTMLElement; // Обработка нажатия кнопки
 areaButton.addEventListener('click', function (event) {
-    const areaList = document.getElementById('divCommentOutputWrapper') as HTMLElement;
-    const comObj  = new CommentObj();
-    comObj.render();
-    //areaList.appendChild("comObj.")
+//  const areaList = document.getElementById('divCommentOutputWrapper') as HTMLElement;
+const comObj  = new CommentObj();
+comObj.render();
+//areaList.appendChild(divCommentInput);
 })
+
+class CommentObj { // Сущность, которая хранит свойства и методы комментария.
+    // Сущность появляется после нажатия кнопки Отправить. Производит отрисовку объекта.
+    avatar: string;
+    nickName: string;
+    timeOfComment: Date;
+    textOfComment: string;
+
+    constructor() {
+        // Создаем объект с параметрами комментария.
+        this.timeOfComment = new Date(); // сохраняем время ввода комментария.
+        const elementAvatar: HTMLElement = document.querySelector(".divAvatar");
+        this.avatar = elementAvatar.getAttribute("background-image");
+        this.nickName = (<HTMLElement>document.getElementById("divNickName")).textContent
+        this.textOfComment = (<HTMLInputElement>document.getElementById("comment")).value
+    }
+
+    render() {
+        const divCommentInput: HTMLElement = document.createElement("divCommentInput");
+        divCommentInput.className ="divCommentInput";
+        const divAvatar: HTMLElement = document.createElement("divAvatar");
+        divAvatar.className = "divAvatar";
+        divCommentInput.appendChild(divAvatar);
+
+        const divCommentInputCenter: HTMLElement = document.createElement("divCommentInputCenter");
+        divCommentInputCenter.className ="divCommentInputCenter";
+        divCommentInput.appendChild(divCommentInputCenter);
+        const divCommentInputTop: HTMLElement = document.createElement("divCommentInputTop");
+        divCommentInputTop.className = "divCommentInputTop";
+        divCommentInputCenter.appendChild(divCommentInputTop);
+
+        const divNickName: HTMLElement = document.createElement("divNickName");
+        divNickName.innerHTML = this.nickName;
+        const divCounter: HTMLElement = document.createElement("divCounter");
+        const divWarning: HTMLElement = document.createElement("divWarning");
+        divCommentInputTop.appendChild(divNickName);
+        divCommentInputTop.appendChild(divCounter);
+        divCommentInputTop.appendChild(divWarning);
+        const divCommentInputBottom: HTMLElement = document.createElement("divCommentInputBottom");
+        divCommentInputBottom.className = "divCommentInputBottom";
+        divCommentInputCenter.appendChild(divCommentInputBottom);
+        const textComment: HTMLElement = document.createElement("textComment");
+        textComment.innerHTML = this.textOfComment;
+        divCommentInputBottom.appendChild(textComment);
+        const divOutputWrapper: HTMLElement = document.querySelector(".divCommentOutputWrapper");
+        divOutputWrapper.appendChild(divCommentInput);
+        return
+        /*
+            <div class="divCommentInput">
+                <div class="divAvatar"></div>
+                <div class="divCommentInputCenter">
+                    <div class="divCommentInputTop">
+                        <div class="divNickName" id="divNickName">
+                            <p style="display: inline">Максим Авдеенко</p>
+                        </div>
+                        <div class="divCounter">
+                            Макс. 1000 символов
+                        </div>
+                        <div class="divWarning"></div>
+                    </div>
+                    <div class="divCommentInputBottom">
+                        <form action="main.js" class="formInput" id="commentForm">
+                            <textarea datatype="text" class="textComment" placeholder="Введите текст сообщения"
+                            name="comment" id="comment" cols="40" rows="3" required form="commentForm"></textarea>
+                            <input class="divButton" id="divButton" type="submit" value="Отправить" disabled>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        */
+    }
+}
+
+//export class ListOfComments extends CommentObj{ // Сущность, которая хранит список комментариев.
+//  ListOfComments: { id: number, avatar: string, nickName: string, textOfComment: string, timeOfComment: Date }[]
+
+//}
+
+//export default CommentObj;
