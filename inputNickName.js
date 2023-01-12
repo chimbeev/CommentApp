ac869745e8d43783214d6c5c64cae9fcd530aa44
Chimba <chimbeevaa@gmail.com>
2023-01-12 22:29:24 +0800

    120123

--- inputNickName.js
+++ inputNickName.js
@@ -4,29 +4,48 @@ export class InputNickName {
     constructor() {
         const areaInputNickName = document.getElementById('inputNickName');
         if (areaInputNickName != null) { //Если есть поле ввода никнейма, то
-            areaInputNickName.addEventListener('blur', function (event) {
-                let target = event.target;
-                let resultOfSearch = localStorage.getItem(target.value); //ищем в локалсторадж по никнейму
-                const divInputNickName = document.querySelector('.divInputNickName');
-                const divAvatar = divInputNickName === null || divInputNickName === void 0 ? void 0 : divInputNickName.querySelector('.divAvatar');
-                if (resultOfSearch) {
-                    console.log("нашел в локалсторадж", JSON.parse(resultOfSearch)); //если находим в локалсторадж
-                    let resObj = JSON.parse(resultOfSearch); // то результат поиска сохраняем в переменную-объект
-                    if (divAvatar)
-                        divAvatar.style.backgroundImage = resObj.avatar;
-                }
-                else //если не находим, то получаем новый аватар
-                 if (divAvatar)
-                    divAvatar.style.backgroundImage = "URL('https://picsum.photos/85/128')";
-                const divLoginButton = document.getElementById('divLoginButton');
-                if (divLoginButton) {
-                    divLoginButton === null || divLoginButton === void 0 ? void 0 : divLoginButton.removeAttribute('disabled'); //открываем для нажатия кнопку Войти
-                    divLoginButton.addEventListener('click', function (event) {
-                        areaInputNickName.setAttribute('disabled', 'true'); //блокируем ввод никнейма
-                        divLoginButton.value = 'Выйти';
-                    });
-                }
-            });
+            const divLoginButton = document.getElementById('divLoginButton');
+            const divInputNickName = document.querySelector('.divInputNickName');
+            const divAvatar = divInputNickName === null || divInputNickName === void 0 ? void 0 : divInputNickName.querySelector('.divAvatar');
+            if (divLoginButton) {
+                divLoginButton.addEventListener('click', function (event) {
+                    let target = event.target;
+                    if (divLoginButton.value == 'Выйти') {
+                        if (divAvatar)
+                            divAvatar.style.backgroundImage = "";
+                        areaInputNickName.disabled = false;
+                        target.value = 'Войти';
+                        divLoginButton.value = 'Войти';
+                    }
+                    else {
+                        let resultOfSearch = localStorage.getItem(areaInputNickName.value); //ищем в локалсторадж по никнейму
+                        areaInputNickName.disabled = true;
+                        //if (resultOfSearch) { console.log("нашел в локалсторадж", JSON.parse(resultOfSearch)); //если находим в локалсторадж
+                        if (resultOfSearch) {
+                            console.log("нашел в локалсторадж", resultOfSearch); //если находим в локалсторадж
+                            const reader = new FileReader();
+                            reader.readAsDataURL(resultOfSearch);
+                            let resObj = JSON.parse(resultOfSearch); // то результат поиска сохраняем в переменную-объект
+                            if (divAvatar)
+                                divAvatar.style.backgroundImage = resObj.avatar;
+                        }
+                        else { //если не находим, то получаем новый аватар
+                            if (divAvatar) {
+                                divAvatar.style.backgroundImage = "URL('https://picsum.photos/85/128')";
+                                console.log("Не нашел в сторадже");
+                                if (divInputNickName)
+                                    localStorage.setItem(areaInputNickName === null || areaInputNickName === void 0 ? void 0 : areaInputNickName.value, divAvatar.style.backgroundImage); //записываем никнейм и аватар
+                                divLoginButton.value = 'Выйти';
+                            }
+                        }
+                    }
+                    //        if (divLoginButton.value = 'Выйти') { if (divAvatar) divAvatar.style.backgroundImage = "";
+                    //areaInputNickName.setAttribute('disabled', 'false');
+                    //          divLoginButton.value = 'Войти';}
+                    //    else { //areaInputNickName.setAttribute('disabled', 'true'); //блокируем ввод никнейма
+                    //      divLoginButton.value = 'Выйти';}
+                });
+            }
         }
     }
 }
--- inputNickName.ts
+++ inputNickName.ts
@@ -6,24 +6,45 @@ export class InputNickName {
     constructor() {
         const areaInputNickName = document.getElementById('inputNickName') as HTMLInputElement | null;
         if (areaInputNickName != null) { //Если есть поле ввода никнейма, то
-            areaInputNickName.addEventListener('blur', function (event) { //Ждем событие ввода
+            const divLoginButton = document.getElementById('divLoginButton') as HTMLInputElement | null;
+            const divInputNickName = document.querySelector('.divInputNickName') as HTMLInputElement | null;
+            const divAvatar = divInputNickName?.querySelector('.divAvatar') as HTMLInputElement | null;
+            if (divLoginButton) { divLoginButton.addEventListener('click', function (event) {
+
                 let target = event.target as HTMLInputElement;
-                let resultOfSearch = localStorage.getItem(target.value); //ищем в локалсторадж по никнейму
-                const divInputNickName = document.querySelector('.divInputNickName') as HTMLInputElement | null;
-                const divAvatar = divInputNickName?.querySelector('.divAvatar') as HTMLInputElement | null;
-                if (resultOfSearch) { console.log("нашел в локалсторадж", JSON.parse(resultOfSearch)); //если находим в локалсторадж
-                    let resObj = JSON.parse(resultOfSearch); // то результат поиска сохраняем в переменную-объект
-                    if (divAvatar) divAvatar.style.backgroundImage = resObj.avatar }
-                else //если не находим, то получаем новый аватар
-                if (divAvatar) divAvatar.style.backgroundImage = "URL('https://picsum.photos/85/128')";
-                const divLoginButton = document.getElementById('divLoginButton') as HTMLInputElement | null;
-                if (divLoginButton) { divLoginButton?.removeAttribute('disabled'); //открываем для нажатия кнопку Войти
-                    divLoginButton.addEventListener('click', function (event) {
-                        areaInputNickName.setAttribute('disabled', 'true'); //блокируем ввод никнейма
-                        divLoginButton.value = 'Выйти';
+                if (divLoginButton.value == 'Выйти') { if (divAvatar) divAvatar.style.backgroundImage = "";
+                    areaInputNickName.disabled = false;
+                    target.value = 'Войти'; divLoginButton.value = 'Войти'}
+                else {
+                    let resultOfSearch = localStorage.getItem(areaInputNickName.value); //ищем в локалсторадж по никнейму
+                    areaInputNickName.disabled = true;
+                    //if (resultOfSearch) { console.log("нашел в локалсторадж", JSON.parse(resultOfSearch)); //если находим в локалсторадж
+                    if (resultOfSearch) { console.log("нашел в локалсторадж", resultOfSearch); //если находим в локалсторадж
+                        const reader = new FileReader();
+                        reader.readAsDataURL(resultOfSearch)
+                        let resObj = JSON.parse(resultOfSearch); // то результат поиска сохраняем в переменную-объект
+                        if (divAvatar) divAvatar.style.backgroundImage = resObj.avatar }
+                    else { //если не находим, то получаем новый аватар
+                        if (divAvatar) { divAvatar.style.backgroundImage = "URL('https://picsum.photos/85/128')";
+                            console.log("Не нашел в сторадже");
+                            if (divInputNickName) localStorage.setItem(areaInputNickName?.value, divAvatar.style.backgroundImage); //записываем никнейм и аватар
+                            divLoginButton.value = 'Выйти'
+                        }
+                        }
+
+                    }
+
+
+
+                //        if (divLoginButton.value = 'Выйти') { if (divAvatar) divAvatar.style.backgroundImage = "";
+                            //areaInputNickName.setAttribute('disabled', 'false');
+                  //          divLoginButton.value = 'Войти';}
+                    //    else { //areaInputNickName.setAttribute('disabled', 'true'); //блокируем ввод никнейма
+                      //      divLoginButton.value = 'Выйти';}
+
                     })
                 }
-            })
+
         }
     }
 }
